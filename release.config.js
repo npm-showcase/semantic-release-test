08457df1230159ccebe62c7a6a9789f067a7c46e
TZ <atian25@qq.com>
2022-12-15 22:03:25 +0800

    feat: plugin

--- release.config.js
+++ release.config.js
@@ -1,9 +1,10 @@
 module.exports = {
+
   plugins: [
     [ '@semantic-release/commit-analyzer', { preset: 'conventionalcommits' } ],
     [ '@semantic-release/release-notes-generator', { preset: 'conventionalcommits' } ],
     [ '@semantic-release/changelog', { changelogTitle: '# Changelog' } ],
-    [ '@semantic-release/npm', {} ],
+    // [ '@semantic-release/npm', {} ],
 
     // [ '@semantic-release/exec', {
     //   'publishCmd': 'ls -l dist && ls -l .semantic-release && tar -ztvf .semantic-release/*.tgz',
@@ -15,16 +16,23 @@ module.exports = {
       },
     ],
 
+    './plugin.js',
     [{
+      analyzeCommits: function (...args) {
+        // console.log('analyzeCommits:', args);
+      },
+      success: function (...args) {
+        console.log('Success:', args);
+      },
       fail: function (...args) {
-        console.log('Hello World!', args);
+        console.log('Fail:', args);
       }
     }],
 
-    [ '@semantic-release/github',
-      {
-        addReleases: 'bottom',
-      },
-    ],
+    // [ '@semantic-release/github',
+    //   {
+    //     addReleases: 'bottom',
+    //   },
+    // ],
   ],
 };
